1d8cb4f557f51f64d7eaecd02780aa030ee7ad25
gsucharitha <100150354+gsucharitha@users.noreply.github.com>
2023-03-21 20:57:46 +0530

    Create iam-main.tf

---
+++ iam-main.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_policy" "policy" {
+  name        = var.iam_policy_for_lambda
+  path        = "/"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+    {
+        "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+        ],
+        "Resource": "arn:aws:logs:*:*:*",
+        "Effect": "Allow"
+    },
+    ]
+  })
+}
